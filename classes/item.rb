cc75613082061bee82cfbfb3b8995d6da9037b41
khanh-devos <dreamproperty.khanh@gmail.com>
2023-09-18 18:05:40 +0700

    modify some attrs in Item

--- classes/item.rb
+++ classes/item.rb
@@ -1,14 +1,19 @@
-require 'date'
+require 'securerandom'
+
 
 class Item
+    attr_reader @published_date
+    attr_accessor @archived
 
     def initialize(published_date, archived = false)
+        @id = SecureRandom.hex
         @published_date = published_date
-        @archived = 
+        @archived = archived
     end
 
+    private
 
-    def can_be_archived?()
+    def can_be_archived?
         today = Time.now
         year, month, day = [today.year, today.month, today.day]
         point_of_10_years_algo = Time.new(year - 10, month, day)
@@ -19,7 +24,9 @@ class Item
         (pdate < point_of_10_years_algo)
     end
 
-    def move_to_archive()
+    public 
+
+    def move_to_archive
         if can_be_archived? then @archived = true end 
     end
 
---
+++ main.rb
@@ -0,0 +1,8 @@
+
+
+def start
+    puts ""
+
+
+
+end

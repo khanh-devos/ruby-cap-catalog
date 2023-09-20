18589a5d3fa641dad2a67e19f618e6b894f9bc77
aqurds <omarf1320@gmail.com>
2023-09-20 15:08:39 +0600

    Add musicAlbum, Genre class

---
+++ classes/genre.rb
@@ -0,0 +1,18 @@
+class Genre
+  include SerializationItem
+  attr_reader :id
+  attr_accessor :name, :items
+
+  def initialize(name)
+    @id = rand 0..100
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    return if @items.include?(item.id)
+
+    @items << item.id
+    item.genre = self
+  end
+end
